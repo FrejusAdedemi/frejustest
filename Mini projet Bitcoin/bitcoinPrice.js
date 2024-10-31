78e7638613b7be711cab7bdd4acfc95c4273de60
Frejus Adedemi <frejusadedemi2@gmail.com>
2024-10-31 10:20:11 +0100

    fr

---
+++ Mini projet Bitcoin/bitcoinPrice.js
@@ -0,0 +1,33 @@
+const url='https://blockchain.info/ticker';
+function getBitcoinPrice() 
+{
+//  creer une requete
+let requete = new XMLHttpRequest();
+
+//  ouvrir la requete
+requete.open('GET', url);// on peut utiliser POST pour autre chose
+requete.responseType = 'json';
+//  envoyer la requete
+requete.send();
+
+//  traiter la reponse
+requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) 
+        {
+            if (requete.status === 200) 
+                {
+                    let response = requete.response;
+                    console.log(response);
+                    let reponse = requete.response;
+                    let prixEnEuros= reponse.EUR.last;
+                    document.querySelector('#price_label').textContent = prixEnEuros;
+                }
+                else
+                {
+                    alert('Un probleme est intervenu. Veuillez reessayer plus tard.');
+                }
+        }
+}
+console.log('prix actualiser');
+}
+setInterval(getBitcoinPrice, 1000);
